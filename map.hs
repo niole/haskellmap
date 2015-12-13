4b3f3baa3e229e2a554f8b808d84fd3116686b0c
Niole Nelson <niolenelson@gmail.com>
2015-12-13 16:58:58 -0500

    actualy maps

--- map.hs
+++ map.hs
@@ -1,8 +1,8 @@
-mp :: [Int] -> [Int]
-mp [] = []
-mp (x:xs) = func (x) : mp (xs)
+mp :: (Int -> Int) -> [Int] -> [Int]
+mp _ [] = []
+mp func (x:xs) = func (x) : mp func (xs)
 
 func :: Int -> Int
 func x = x*3
 
-main = print (mp ([1,2,3]))
+main = print (mp func [1,2,3])
